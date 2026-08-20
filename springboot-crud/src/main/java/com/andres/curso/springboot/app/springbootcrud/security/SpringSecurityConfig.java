f2e70e3cc22345e6997725e535ac7b39c7091a2a
Mor4n <anm0ran@outlook.com>
2026-08-20 15:25:03 -0700

    Actualización de filtros d espring securty para dar acceso a Swagger

--- springboot-crud/src/main/java/com/andres/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
+++ springboot-crud/src/main/java/com/andres/curso/springboot/app/springbootcrud/security/SpringSecurityConfig.java
@@ -46,6 +46,7 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests((authz) -> authz
                 .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                 // .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
                 // .requestMatchers(HttpMethod.GET, "/api/products", "/api/products/{id}").hasAnyRole("ADMIN", "USER")
                 // .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
